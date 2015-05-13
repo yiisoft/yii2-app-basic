ff0a3944bfb5edfd38fa8989603778178f186191
Evgeniy Tkachenko <et.coder@gmail.com>
2015-05-13 14:14:06 +0300

    Update config.php

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -3,6 +3,7 @@
  * Application configuration shared by all test types
  */
 return [
+    'language' => 'en-US',
     'controllerMap' => [
         'fixture' => [
             'class' => 'yii\faker\FixtureController',
