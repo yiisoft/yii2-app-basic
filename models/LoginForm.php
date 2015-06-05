a52ca24ecd14aacb307a5dd011ff2dc6690e0371
MaximAL <almaximal@ya.ru>
2015-06-05 17:40:49 +0300

    Update LoginForm.php

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -58,9 +58,8 @@ class LoginForm extends Model
     {
         if ($this->validate()) {
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
-        } else {
-            return false;
         }
+        return false;
     }
 
     /**
