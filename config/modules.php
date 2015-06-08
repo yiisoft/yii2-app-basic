91763832b825f682cbbc5ecc75ceb61b773b64c0
callmez <callme-z@qq.com>
2015-06-08 23:16:23 +0800

    管理员ID

--- config/modules.php
+++ config/modules.php
@@ -2,7 +2,8 @@
 return [
     // 微信模块
     'wechat' => [
-        'class' => 'callmez\wechat\Module'
+        'class' => 'callmez\wechat\Module',
+        'adminId' => [100, 101] // 这里填写管理员ID(拥有wechat最高管理权限)
     ],
     'example' => [ // 微信扩展示例模块, 生产环境可删除
         'class' => 'app\modules\example\Module',
