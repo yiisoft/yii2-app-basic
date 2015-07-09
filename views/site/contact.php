4bf44f5eae41b5d40b0c61accd9133538e4db489
Alex Solomaha <cyanofresh@gmail.com>
2015-07-09 13:26:00 +0300

    Contact Indentation fix

--- views/site/contact.php
+++ views/site/contact.php
@@ -25,10 +25,9 @@ $this->params['breadcrumbs'][] = $this->title;
             to view the mail message on the mail panel of the debugger.
             <?php if (Yii::$app->mailer->useFileTransport): ?>
                 Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                                                                                                    Please configure the
-                <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
+                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>. Please configure the
+                <code>useFileTransport</code> property of the <code>mail</code> application component to be false to
+                enable email sending.
             <?php endif; ?>
         </p>
 
