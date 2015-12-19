4884fc1991140d991332b442988c4a105cedcf72
cohen <cohen@huoban.com>
2015-12-19 18:48:33 +0800

    save time and resource of cpu

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -57,7 +57,8 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            // 2592000 = 3600*24*30
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 2592000 : 0);
         }
         return false;
     }
