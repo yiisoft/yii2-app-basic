f002567220b9ce9670f4d7fd71701df17c87a86b
Alexander Makarov <sam@rmcreative.ru>
2016-03-09 18:47:19 +0300

    Removed unnecessary slash

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -54,7 +54,7 @@ class SiteController extends Controller
 
     public function actionLogin()
     {
-        if (!\Yii::$app->user->isGuest) {
+        if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
 
