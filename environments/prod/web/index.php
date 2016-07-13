2321fc09ba3b3793b01aeadcee3321221a2446bc
Vagrant User <vagrant@yii2basic.fork.puphpet>
2016-07-13 12:14:41 +0300

    Created Vagrant support and Environments

---
+++ environments/prod/web/index.php
@@ -0,0 +1,11 @@
+<?php
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/../config/web.php');
+
+$application = new yii\web\Application($config);
+$application->run();
