b7f782ddbeeb46f56f398f61047fdee745f92493
MaximAL <almaximal@ya.ru>
2016-07-16 14:06:45 +0300

    Convenient way to set up `allowedIPs` for `debug` and `gii` modules

--- config/web.php
+++ config/web.php
@@ -55,11 +55,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => $config['modules']['debug']['allowedIPs'],
     ];
 }
 
