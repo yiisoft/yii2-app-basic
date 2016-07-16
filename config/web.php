37c9dfcfd8d1ccb3900c2f72f11837bc31666c15
Davert <davert.php@resend.cc>
2016-07-16 04:30:11 +0300

    removed test key from config, removed generated actions traits

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '12334543543',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
