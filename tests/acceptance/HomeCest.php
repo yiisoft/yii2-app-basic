3971d0bb3a562dcdd3484f542ff42a9faaf9593b
Елисеев Дмитрий <mail@elisdn.ru>
2016-10-19 16:42:29 +0300

    Fixed acceptance HomeCest (#91)

--- tests/acceptance/HomeCest.php
+++ tests/acceptance/HomeCest.php
@@ -10,6 +10,7 @@ class HomeCest
         
         $I->seeLink('About');
         $I->click('About');
+        $I->wait(2); // wait for page to be opened
         
         $I->see('This is the About page.');
     }
