b3e97d79406ae1b74dc66acbbf76d516fb8d81a2
Carsten Brandt <mail@cebe.cc>
2016-11-23 13:58:17 +0100

    Update web.php
    to include example of 'allowedIps' for gii and debug module.
    
    fixes https://github.com/yiisoft/yii2-debug/pull/30

--- config/web.php
+++ config/web.php
@@ -55,11 +55,15 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
