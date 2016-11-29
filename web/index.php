a08679e5d6325e4e51847311c5b854f7ad9b89e6
Carsten Brandt <mail@cebe.cc>
2016-11-29 12:57:45 +0100

    proposal for DI container config
    issue https://github.com/yiisoft/yii2/issues/11758

--- web/index.php
+++ web/index.php
@@ -4,9 +4,13 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+// require composer and Yii autoloaders
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+// configure dependency injection container
+require(__DIR__ . '/../config/di.php');
 
+// configure and create application
+$config = require(__DIR__ . '/../config/web.php');
 (new yii\web\Application($config))->run();
