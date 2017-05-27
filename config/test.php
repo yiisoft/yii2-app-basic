465c00a8eac7251ca2ae521c682ab6467ce1fe9e
Andrey Subbota <subbota@gmail.com>
2017-05-28 02:21:16 +0400

    Fixed consistency of db-variable name in config files (#120)

--- config/test.php
+++ config/test.php
@@ -1,6 +1,6 @@
 <?php
 $params = require(__DIR__ . '/params.php');
-$dbParams = require(__DIR__ . '/test_db.php');
+$db = require(__DIR__ . '/test_db.php');
 
 /**
  * Application configuration shared by all test types
@@ -10,7 +10,7 @@ return [
     'basePath' => dirname(__DIR__),    
     'language' => 'en-US',
     'components' => [
-        'db' => $dbParams,
+        'db' => $db,
         'mailer' => [
             'useFileTransport' => true,
         ],
