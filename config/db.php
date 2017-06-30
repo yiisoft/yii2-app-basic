90b51ff06a8f01347a9a45a0d39583037af7d461
MaximAL <almaximal@ya.ru>
2017-06-30 18:00:32 +0300

    Schema cache options (for production environment) (#129) [skip ci]

--- config/db.php
+++ config/db.php
@@ -6,4 +6,9 @@ return [
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
 ];
