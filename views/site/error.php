af1098ebcdc78f4cede1778d8732f7ada2c8d171
Bulat Damdinov <dbulats88@gmail.com>
2017-07-22 21:33:10 +0800

    Minor fixes

--- views/site/error.php
+++ views/site/error.php
@@ -1,13 +1,13 @@
 <?php
 
+use yii\helpers\Html;
+
 /**
  * @var \yii\web\View $this
  * @var string $name
  * @var string $message
- * @var Exception $exception
+ * @var \Exception $exception
  */
-
-use yii\helpers\Html;
 
 $this->title = $name;
 ?>
