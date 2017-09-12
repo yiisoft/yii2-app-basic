d965cf55ac85eb4372412b1ae65bec723bb3f7ce
Bulat Damdinov <dbulats88@gmail.com>
2017-09-12 17:16:50 +0800

    Using require as a statement (#140)

--- web/index.php
+++ web/index.php
@@ -4,9 +4,9 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-$config = require(__DIR__ . '/../config/web.php');
+$config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
