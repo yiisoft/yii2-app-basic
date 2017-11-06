ec9d73e21deb02b17c082fbbce4f065e73b60b16
Milen Hristov <milen@systemhaus-v.com>
2017-11-06 10:40:26 +0200

    since 2.0.13, the class name `Object` is invalid since PHP 7.2, use [[BaseObject]] instead.

--- models/User.php
+++ models/User.php
@@ -2,7 +2,7 @@
 
 namespace app\models;
 
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
+class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
 {
     public $id;
     public $username;
