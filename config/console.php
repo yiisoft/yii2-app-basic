dafe5f6afff00ca4990c1aeda6a1d78512f565eb
Ricardo Pérez <ricpelo@gmail.com>
2018-02-23 18:57:48 +0100

    Set `@tests` alias by default (#169)

--- config/console.php
+++ config/console.php
@@ -11,6 +11,7 @@ $config = [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
+        '@tests' => '@app/tests',
     ],
     'components' => [
         'cache' => [
