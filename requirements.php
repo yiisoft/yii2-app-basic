36365e5aa7e80f01634ddba3b56c2db3f76680f3
MaximAL <almaximal@ya.ru>
2019-03-06 20:01:30 +0300

    Make requirements checker return error if there were failed checks (#196)

--- requirements.php
+++ requirements.php
@@ -157,4 +157,6 @@ if (!version_compare(phpversion(), '5.5', '>=')) {
     );
 }
 
-$requirementsChecker->checkYii()->check($requirements)->render();
+$result = $requirementsChecker->checkYii()->check($requirements)->getResult();
+$requirementsChecker->render();
+exit($result['summary']['errors'] === 0 ? 0 : 1);
