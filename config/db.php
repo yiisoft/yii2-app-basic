83c8ab10963daab7be03f2633640c8ab5b125e82
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2020-01-18 11:50:27 +0900

    added mysql container to docker platform.

--- config/db.php
+++ config/db.php
@@ -5,7 +5,12 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+    
+    // uncomment this section, if you want to use db(mysql) container.
+    // 'dsn' => 'mysql:host=db;dbname=yii2basic',
+    // 'username' => 'user',
+    // 'password' => 'password',
+    // 'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
