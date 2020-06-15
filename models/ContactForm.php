aeea0d89652f6c7905b1ba61a32ea82ebd65e01c
Said Bakr <said.fox@gmail.com>
2020-06-15 18:11:39 +0200

    Temporary remove names from emails
    Temporary remove names from `setFrom()` and `setReplyTo()` to allow correct sending contact form by default without errors, till the related issue of [swiftMailer solved](https://github.com/yiisoft/yii2-swiftmailer/issues/51)

--- models/ContactForm.php
+++ models/ContactForm.php
@@ -52,8 +52,8 @@ class ContactForm extends Model
         if ($this->validate()) {
             Yii::$app->mailer->compose()
                 ->setTo($email)
-                ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
-                ->setReplyTo([$this->email => $this->name])
+                ->setFrom(Yii::$app->params['senderEmail']) // Temporary remove Yii::$app->params['senderName']
+                ->setReplyTo($this->email) // Temporary remove $this->name
                 ->setSubject($this->subject)
                 ->setTextBody($this->body)
                 ->send();
