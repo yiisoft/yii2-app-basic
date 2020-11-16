a56693d88f893595d902c55f399355b80acbbac2
Alexander Makarov <sam@rmcreative.ru>
2020-11-16 16:22:10 +0300

    Do not mock contact form in test

--- tests/unit/models/ContactFormTest.php
+++ tests/unit/models/ContactFormTest.php
@@ -7,7 +7,6 @@ use yii\mail\MessageInterface;
 
 class ContactFormTest extends \Codeception\Test\Unit
 {
-    private $model;
     /**
      * @var \UnitTester
      */
@@ -15,23 +14,17 @@ class ContactFormTest extends \Codeception\Test\Unit
 
     public function testEmailIsSentOnContact()
     {
-        /** @var ContactForm $model */
-        $this->model = $this->getMockBuilder('app\models\ContactForm')
-            ->setMethods(['validate'])
-            ->getMock();
+        $model = new ContactForm();
 
-        $this->model->expects($this->once())
-            ->method('validate')
-            ->willReturn(true);
-
-        $this->model->attributes = [
+        $model->attributes = [
             'name' => 'Tester',
             'email' => 'tester@example.com',
             'subject' => 'very important letter subject',
             'body' => 'body of current message',
+            'verifyCode' => 'testme',
         ];
 
-        expect_that($this->model->contact('admin@example.com'));
+        expect_that($model->contact('admin@example.com'));
 
         // using Yii2 module actions to check email was sent
         $this->tester->seeEmailIsSent();
