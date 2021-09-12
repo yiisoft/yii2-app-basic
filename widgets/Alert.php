7c6d1569493fca1184ead0b5f51b7a6c45f2cc44
Tobias Dietz <tobi@swindi.de>
2021-09-12 12:07:39 +0200

    Make alert widget not access all flash data (Fixes #250)

--- widgets/Alert.php
+++ widgets/Alert.php
@@ -51,21 +51,18 @@ class Alert extends \yii\bootstrap4\Widget
     public function run()
     {
         $session = Yii::$app->session;
-        $flashes = $session->getAllFlashes();
         $appendClass = isset($this->options['class']) ? ' ' . $this->options['class'] : '';
 
-        foreach ($flashes as $type => $flash) {
-            if (!isset($this->alertTypes[$type])) {
-                continue;
-            }
+        foreach (array_keys($this->alertTypes) as $type) {
+            $flash = $session->getFlash($type);
 
             foreach ((array) $flash as $i => $message) {
                 echo \yii\bootstrap4\Alert::widget([
                     'body' => $message,
                     'closeButton' => $this->closeButton,
                     'options' => array_merge($this->options, [
-                        'id' => $this->getId() . '-' . $type . '-' . $i,
-                        'class' => $this->alertTypes[$type] . $appendClass,
+                        'id' => $this->getId().'-'.$type.'-'.$i,
+                        'class' => $this->alertTypes[$type].$appendClass,
                     ]),
                 ]);
             }
