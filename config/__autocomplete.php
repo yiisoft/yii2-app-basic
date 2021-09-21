a2bd73f22aa7122335599923a5307c923a7ff7ba
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2021-09-21 14:25:50 +0530

    Update __autocomplete.php

--- config/__autocomplete.php
+++ config/__autocomplete.php
@@ -3,6 +3,13 @@
 /**
  * This class only exists here for IDE (PHPStorm/Netbeans/...) autocompletion.
  * This file is never included anywhere.
+ * Adjust this file to match classes configured in your application config, to enable IDE autocompletion for custom components.
+ * Example: A property phpdoc can be added in `__Application` class as `@property \vendor\package\Rollbar|__Rollbar $rollbar` and adding a class in this file
+ * ```php
+ * // @property of \vendor\package\Rollbar goes here
+ * class __Rollbar {
+ * }
+ * ```
  */
 class Yii {
     /**
@@ -12,9 +19,9 @@ class Yii {
 }
 
 /**
- * @property yii\rbac\DbManager $authManager
- * @property yii\queue\redis\Queue $queue
+ * @property yii\rbac\DbManager $authManager 
  * @property \yii\web\User|__WebUser $user
+ * 
  */
 class __Application {
 }
