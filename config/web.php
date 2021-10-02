e0a8e5d40760bd2b23489cf62e4f60aa836a6f15
Toir Tuychiev <toir427@gmail.com>
2021-10-02 11:13:28 +0500

    typo fixes

--- config/web.php
+++ config/web.php
@@ -29,7 +29,7 @@ $config = [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
+            // 'useFileTransport' to false and configure transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
