479a83cc94518ee148f96338eda53c1119a19578
Alexander Makarov <sam@rmcreative.ru>
2021-12-30 10:59:54 +0300

    release version 2.0.44

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -8,7 +8,7 @@ use yii\base\Model;
 /**
  * LoginForm is the model behind the login form.
  *
- * @property-read User|null $user This property is read-only.
+ * @property-read User|null $user
  *
  */
 class LoginForm extends Model
