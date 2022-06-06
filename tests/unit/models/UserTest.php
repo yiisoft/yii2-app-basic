061fba9f7e7abd1594ed8d3c7ace20ec69a2367d
Luke English <luke@developedsoftware.com>
2022-06-06 15:09:57 +0100

    invalid test

--- tests/unit/models/UserTest.php
+++ tests/unit/models/UserTest.php
@@ -31,7 +31,7 @@ class UserTest extends \Codeception\Test\Unit
     /**
      * @depends testFindUserByUsername
      */
-    public function testValidateUser($user)
+    public function testValidateUser()
     {
         $user = User::findByUsername('admin');
         verify($user->validateAuthKey('test100key'))->notEmpty();
