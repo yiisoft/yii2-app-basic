bb291ea536c70975f3fdc3cb216c1fe5bf46ac94
Luke English <luke@developedsoftware.com>
2022-06-07 10:53:00 +0100

    replaced swiftmailer

--- config/test.php
+++ config/test.php
@@ -16,10 +16,9 @@ return [
     'components' => [
         'db' => $db,
         'mailer' => [
-            'class' => 'yii\symfonymailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure transport
-            // for the mailer to send real emails.
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@app/mail',
+            // send all mails to a file by default.
             'useFileTransport' => true,
         ],
         'assetManager' => [
