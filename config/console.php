ba107f7494621dfba516169b3812e6e43e22bad4
Basil <git@nadar.io>
2022-10-13 22:19:53 +0200

    Add debugger to console (#277)

--- config/console.php
+++ config/console.php
@@ -43,6 +43,14 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
     ];
+    // configuration adjustments for 'dev' environment
+    // requires version `2.1.21` of yii2-debug module
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
 }
 
 return $config;
