13b4039024eee228c9a82c4167d78f852e4da8c3
Wilmer Arambula <terabytesoftw@gmail.com>
2023-07-11 06:09:52 -0400

    Fix `.scrutinizer.yml`.

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '1',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
