43904ee9b3b47a8b4f93d647dc3f19440910e122
Wilmer Arambula <terabytesoftw@gmail.com>
2023-07-11 07:25:10 -0400

    Fix minor correction.

--- web/index-test.php
+++ web/index-test.php
@@ -8,7 +8,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require __DIR__ . '../c3.php';
+require __DIR__ . '/../c3.php';
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
