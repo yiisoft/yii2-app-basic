da6b5f581c74ff86818947ee643b01fff148d7f9
Wilmer Arambula <terabytesoftw@gmail.com>
2025-05-26 11:21:18 -0400

    Fix error type.

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'your cokie validation key here',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
